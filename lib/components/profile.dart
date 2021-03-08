99c9b214df9053db45d28e31b6efd39869a74450
samaralii <samarali@live.com>
2021-03-08 22:06:05 +0500

    custom clipping

--- lib/components/profile.dart
+++ lib/components/profile.dart
@@ -66,15 +66,17 @@ class _ProfileState extends State<Profile> {
   }
 }
 
+// for more info about custom clip path check this article https://medium.com/flutter-community/flutter-custom-clipper-28c6d380fdd6
 class CustomClipPath extends CustomClipper<Path> {
   var radius = 10.0;
   @override
   Path getClip(Size size) {
     Path path = Path();
-    //x w -- y h |
-    path.lineTo(size.width, 20);
-    path.lineTo(size.width, size.height);
-    path.lineTo(0, size.height - 20);
+    path.moveTo(size.width, 0); // start with top right corner
+    path.lineTo(size.width,
+        size.height - 20); // top right corner to buttom right corner
+    path.lineTo(0, size.height); // bottom right corner to bottom left corner
+    path.lineTo(0, 20); // bottom left corner to top left corner
     path.close();
 
     return path;
